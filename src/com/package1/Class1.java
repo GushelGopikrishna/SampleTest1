48adbb570ae643efd53b71984888a3244cac07f1
GushelGopikrishna <gushelgopikrishna96@outlook.com>
2023-01-04 11:50:55 +0530

    About to add project to master

---
+++ src/com/package1/Class1.java
@@ -0,0 +1,18 @@
+package com.package1;
+
+public class Class1 {
+	
+	public String method1() {
+		String message ="Hello All - Welcome to 2023";
+		System.out.println(message);
+		return message;
+		
+	}
+
+	public static void main(String[] args) {
+		Class1 c = new Class1();
+		c.method1();
+
+	}
+
+}
