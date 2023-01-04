543d072b77441c74f4deabfb41b27268b3926add
GushelGopikrishna <gushelgopikrishna96@outlook.com>
2023-01-04 12:41:40 +0530

    2 new lines added

--- src/com/package1/Class1.java
+++ src/com/package1/Class1.java
@@ -4,7 +4,9 @@ public class Class1 {
 	
 	public String method1() {
 		String message ="Hello All - Welcome to 2023";
+		System.out.println("Wish you a happy new year");
 		System.out.println(message);
+		System.out.println("Thank you");
 		return message;
 		
 	}
